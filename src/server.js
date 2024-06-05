3a74aef954f81ca2dbceb5c2b9a51bcd8622226d
mdawlad29 <mdawladbdinfo@gmail.com>
2024-06-06 01:15:42 +0600

    cors error

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
 import createError from "http-errors";
@@ -12,6 +13,7 @@ dotenv.config();
 const port = process.env.PORT || 5000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 
